424cba8a937560b3a0136d8d1ebc91c2a8c7274b
Stefan Mesken <kappadistributive@gmail.com>
2020-04-06 18:24:32 +0200

    implement GetFirstTouchpointValue

--- classical_test.go
+++ classical_test.go
@@ -6,6 +6,51 @@ import (
 	"testing"
 )
 
+func ExampleGetFirstTouchpointValue() {
+	contributions := []Contribution{
+		Contribution{
+			Touchpoints: []Touchpoint{
+				Touchpoint{"Touchpoint 1"},
+			},
+			Value: *new(big.Float).SetFloat64(100.),
+		},
+		Contribution{
+			Touchpoints: []Touchpoint{
+				Touchpoint{"Touchpoint 1"},
+				Touchpoint{"Touchpoint 2"},
+			},
+			Value: *new(big.Float).SetFloat64(200.),
+		},
+		Contribution{
+			Touchpoints: []Touchpoint{
+				Touchpoint{"Touchpoint 1"},
+				Touchpoint{"Touchpoint 3"},
+				Touchpoint{"Touchpoint 1"},
+			},
+			Value: *new(big.Float).SetFloat64(300.),
+		},
+	}
+	touchpoint := Touchpoint{"Touchpoint 1"}
+	firstTouchpointValue := GetFirstTouchpointValue(touchpoint, contributions)
+
+	fmt.Println(firstTouchpointValue.String())
+	// Output: 600
+}
+
+func TestGetFirstTouchpointValue(t *testing.T) {
+	contributions := contributionFixture()
+	touchpoint := touchpointFixture()[2]
+	firstTouchpointValue := GetFirstTouchpointValue(touchpoint, contributions)
+	expectedValue := *(new(big.Float).SetFloat64(1000.))
+
+	got, _ := firstTouchpointValue.Float64()
+	want, _ := expectedValue.Float64()
+
+	if got != want {
+		t.Errorf("got %f want %f", got, want)
+	}
+}
+
 func ExampleGetLastTouchpointValue() {
 	contributions := []Contribution{
 		Contribution{
